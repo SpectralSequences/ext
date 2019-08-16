fff9d387c943ef431ced5f9104a41cfd85ac5d7f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-16 00:21:55 +0000

    Deploy hoodmane/rust_ext to github.com/hoodmane/rust_ext.git:gh-pages

---
+++ doc/implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rust_ext"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.98/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"rust_ext/fp_vector/enum.FpVector.html\" title=\"enum rust_ext::fp_vector::FpVector\">FpVector</a>",synthetic:false,types:["rust_ext::fp_vector::FpVector"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
