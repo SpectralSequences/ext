a3c819a50606114109dbc87e12ea4ade6f10f5fa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-19 03:23:39 +0000

    Deploy hoodmane/rust_ext to github.com/hoodmane/rust_ext.git:gh-pages

---
+++ doc/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ext_websocket"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"ext_websocket/sseq/enum.ClassState.html\" title=\"enum ext_websocket::sseq::ClassState\">ClassState</a>",synthetic:false,types:["ext_websocket::sseq::ClassState"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"ext_websocket/actions/enum.Recipient.html\" title=\"enum ext_websocket::actions::Recipient\">Recipient</a>",synthetic:false,types:["ext_websocket::actions::Recipient"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"ext_websocket/actions/enum.SseqChoice.html\" title=\"enum ext_websocket::actions::SseqChoice\">SseqChoice</a>",synthetic:false,types:["ext_websocket::actions::SseqChoice"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
