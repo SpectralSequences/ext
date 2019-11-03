6ecffd63c7eca219f4fbdc352be2004143048623
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-03 13:59:25 +0000

    Deploy hoodmane/rust_ext to github.com/hoodmane/rust_ext.git:gh-pages

---
+++ doc/implementors/core/ops/arith/trait.Mul.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rust_ext"] = [{text:"impl&lt;'_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;&amp;'_ <a class=\"struct\" href=\"rust_ext/matrix/struct.Matrix.html\" title=\"struct rust_ext::matrix::Matrix\">Matrix</a>&gt; for &amp;'_ <a class=\"struct\" href=\"rust_ext/matrix/struct.Matrix.html\" title=\"struct rust_ext::matrix::Matrix\">Matrix</a>",synthetic:false,types:["rust_ext::matrix::Matrix"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
