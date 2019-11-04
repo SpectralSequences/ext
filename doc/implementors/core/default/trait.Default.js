9de1e6952f782d91c62d0688e286a04d900b2763
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-04 15:59:07 +0000

    Deploy hoodmane/rust_ext to github.com/hoodmane/rust_ext.git:gh-pages

--- doc/implementors/core/default/trait.Default.js
+++ doc/implementors/core/default/trait.Default.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["once"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"once/struct.OnceVec.html\" title=\"struct once::OnceVec\">OnceVec</a>&lt;T&gt;",synthetic:false,types:["once::OnceVec"]},];
 implementors["rust_ext"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rust_ext/algebra/milnor_algebra/struct.QPart.html\" title=\"struct rust_ext::algebra::milnor_algebra::QPart\">QPart</a>",synthetic:false,types:["rust_ext::algebra::milnor_algebra::QPart"]},];
 
             if (window.register_implementors) {
