205e2c90bbec97b52f3cc7d3bf64861e172144e2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-02 18:15:31 +0000

    Deploy hoodmane/rust_ext to github.com/hoodmane/rust_ext.git:gh-pages

--- doc/implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ doc/implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["once"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"once/struct.OnceVec.html\" title=\"struct once::OnceVec\">OnceVec</a>&lt;T&gt;",synthetic:false,types:["once::OnceVec"]},];
+implementors["once"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"once/struct.OnceVec.html\" title=\"struct once::OnceVec\">OnceVec</a>&lt;T&gt;","synthetic":false,"types":["once::OnceVec"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/ops/arith/trait.Mul.js
+++ doc/implementors/core/ops/arith/trait.Mul.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["fp"] = [{text:"impl&lt;'_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;&amp;'_ <a class=\"struct\" href=\"fp/matrix/struct.Matrix.html\" title=\"struct fp::matrix::Matrix\">Matrix</a>&gt; for &amp;'_ <a class=\"struct\" href=\"fp/matrix/struct.Matrix.html\" title=\"struct fp::matrix::Matrix\">Matrix</a>",synthetic:false,types:["fp::matrix::Matrix"]},];
+implementors["fp"] = [{"text":"impl&lt;'_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;&amp;'_ <a class=\"struct\" href=\"fp/matrix/struct.Matrix.html\" title=\"struct fp::matrix::Matrix\">Matrix</a>&gt; for &amp;'_ <a class=\"struct\" href=\"fp/matrix/struct.Matrix.html\" title=\"struct fp::matrix::Matrix\">Matrix</a>","synthetic":false,"types":["fp::matrix::Matrix"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
